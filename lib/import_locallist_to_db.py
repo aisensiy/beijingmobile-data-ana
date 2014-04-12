2e7ad31bb53b9ec779c61ca309d1b2cb0b040fc6
aisensiy <aisensiy@gmail.com>
2014-04-12 16:55:58 +0800

    add argv for save date

--- lib/import_locallist_to_db.py
+++ lib/import_locallist_to_db.py
@@ -1,12 +1,13 @@
 import MySQLdb as mysql
 import pandas as pd
 import pandas.io.sql as psql
-from constants import db
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
+from constants import db
 
 def create_connect_by_mysqldb(config):
     return mysql.connect(**db['dev'])
+
 
 def import_to_db(conn, df):
     try:
@@ -17,10 +18,14 @@ def import_to_db(conn, df):
         conn.close()
     print 'mysql done'
 
+
 if __name__ == '__main__':
     import sys
     input_file = sys.argv[1]
+    date = sys.argv[2]
+
     df = pd.read_csv(input_file)
+    df['date'] = date
     df = df.where(pd.notnull(df), None)
     conn = create_connect_by_mysqldb(db['dev'])
     import_to_db(conn, df)
