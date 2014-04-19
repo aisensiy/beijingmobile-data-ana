9e1dd7632288f14df4d0f381a31aacde2c4ed26a
aisensiy <aisensiy@gmail.com>
2014-04-19 16:08:34 +0800

    format

--- lib/constants.py
+++ lib/constants.py
@@ -1,15 +1,16 @@
 # -*- coding: utf8 -*-
 
 
-log_headers = ['user_id', 'access_mode_id', 'logic_area_name', 'lac', 'ci',
-           'longitude', 'latitude', 'busi_name', 'busi_type_name',
-           'app_name', 'app_type_name', 'start_time',
-           'up_pack', 'down_pack', 'up_flow', 'down_flow', 'site_name',
-           'site_channel_name', 'cont_app_id', 'cont_classify_id',
-           'cont_type_id', 'acce_url']
+log_headers = ['user_id', 'access_mode_id', 'logic_area_name', 'lac',
+               'ci', 'longitude', 'latitude', 'busi_name',
+               'busi_type_name', 'app_name', 'app_type_name', 'start_time',
+               'up_pack', 'down_pack', 'up_flow', 'down_flow',
+               'site_name', 'site_channel_name', 'cont_app_id', 'cont_classify_id',
+               'cont_type_id', 'acce_url']
 
-call_headers = ['user_id', 'target_id', 'start_time', 'end_time', 'roam',
-                'basename', 'longitude', 'latitude']
+call_headers = ['user_id', 'target_id', 'start_time',
+                'end_time', 'roam', 'basename',
+                'longitude', 'latitude']
 
 locallist_headers = ['user_id', 'locations', 'location_size', 'date']
 
